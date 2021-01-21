d57cabe7769163eac13af6cce1b37c3112eb952f
EdilsonBaggio <edilsonbaggio_@hotmail.com>
2021-01-21 13:09:50 -0300

    Atualização de conteudos

--- my-a/src/App.jsx
+++ my-a/src/App.jsx
@@ -2,13 +2,13 @@ import './App.css'
 
 import React from 'react'
 
-import Primero from './componentes/Primeiro'
-import Comparametro from './componentes/Comparametro'
-import Comfilhos from './componentes/Comfilhos'
+import Primero from './componentes/basicos/Primeiro'
+import Comparametro from './componentes/basicos/Comparametro'
+import Comfilhos from './componentes/basicos/Comfilhos'
 import Card from './componentes/layout/Card'
 import Repeticao from './componentes/Repeticao'
-import Condicional from './componentes/Condicional'
-import CondicionalComIf from './componentes/CondicionalComIf'
+import Condicional from './componentes/basicos/Condicional'
+import CondicionalComIf from './componentes/basicos/CondicionalComIf'
 
 export default (props) => (
 
@@ -38,16 +38,6 @@ export default (props) => (
         <Card titulo="Primeiro componente">
           <Primero/>
         </Card>
-        {/*<Comfilhos>
-            <ul>
-                <li>Home</li>
-                <li>Blog</li>
-                <li>Trabalhos</li>
-                <li>Contatos</li>
-            </ul>
-        </Comfilhos>**/}
-        {/*<Primero/>
-        <Comparametro titulo="Este é o titulo" subtitulo="este é o subtitulo"/>*/}
     </div>
 
 )
--- my-a/src/componentes/layout/Card.jsx
+++ my-a/src/componentes/layout/Card.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default props =>
 
    <div className="card">
-        <div className="conteudo">
+        <div className="content">
             {props.children}
         </div>
 
